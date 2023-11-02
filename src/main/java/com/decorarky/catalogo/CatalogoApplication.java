5a0078c8eb0496b6f46065ec5997175ae7f69143
IngAndresCasas <ingfabiocasas@gmail.com>
2023-11-02 16:49:49 -0500

    primer commit

--- src/main/java/com/decorarky/catalogo/CatalogoApplication.java
+++ src/main/java/com/decorarky/catalogo/CatalogoApplication.java
@@ -3,15 +3,16 @@ package com.decorarky.catalogo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CatalogoApplication {
+public class CatalogoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
 	}
 
-
+@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CatalogoApplication.class);
 	}
